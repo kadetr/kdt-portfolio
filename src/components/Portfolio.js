cfe966ba80cfe034becf948aff05f91af3adc215
Kemal Deniz Teket <kemaldenizteket@Kemals-MacBook-Pro.local>
2023-04-13 14:08:09 +0300

    initial commit

---
+++ src/components/Portfolio.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+export default class Porfolio extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+        <div className="row skill">
+
+        <div className="three columns header-col">
+          <h1><span>Skills</span></h1>
+        </div>
+
+        <div className="nine columns main-col">
+
+          <div className="bars">
+
+            <ul className="skills">
+                {
+                  resumeData.skills && resumeData.skills.map((item) => {
+                    return(
+                      <li>
+                      <span className={`bar-expand ${item.skillname.toLowerCase()}`}>
+                      </span><em>{item.skillname}</em>
+                      </li>
+                    )
+                  })
+                }
+
+            </ul>
+
+        </div>
+
+        </div>
+        <p><b>Other Technologies Worked with: </b>
+          {resumeData.skillsDescription}
+        </p>
+
+        </div>
+        </div>
+      </div>
+  </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,156 @@
+let resumeData = {
+    "imagebaseurl":"trees.jpg",
+    "name": "Kemal Deniz Teket",
+    "role": "Fullstack Developer & Researcher",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like working with various technologies of fullstack development. The ones that I used most are Nodejs, React, Javascript, Typescript. I also like to discover new technologies, recently I started playing with Docker, Prisma, Postgres for an educational project.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/kemal-deniz-teket-7a78ab17/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/kadetr",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am a full-stack developer since 2021, mainly skilled in MERN stack, Javascript and Typescript I also have 3 years of research experience in p2p networks (C/C++, ns-3). My previous position was fullstack developer for a healthcare company (via Toptal).",
+    "address":"Izmir - Turkiye",
+    "website":"http://kemaldenizteket.com",
+    "email":"kemaldeniz.teket@gmail.com",
+    "education":[
+      {
+        "UniversityName":"Ege University",
+        "specialization":"MSc in Computer Science",
+        "MonthOfPassing":"November",
+        "YearOfPassing":"2013",
+        "Achievements":"member of an academic project team for 2 years, contributed to several IEEE proceedings & SCI journal"
+      },
+      {
+        "UniversityName":"Galatasaray University",
+        "specialization":"Computer Engineering",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2008"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Perculus",
+        "specialization":"Fullstack Developer",
+        "MonthOfLeaving":"April",
+        "YearOfLeaving":"2023",
+        "Tools": "HTML, CSS, Javascript, Typescript, React, Nodejs, Sqlite, Postgres, Docker, JIRA, Linux",
+        "Achievements":"Admin portal implementation for media server, database migration for media server"
+      },
+      {
+        "CompanyName":"A Healthcare Company via Toptal",
+        "specialization":"Freelance Fullstack Developer",
+        "MonthOfLeaving":"April",
+        "YearOfLeaving":"2022",
+        "Tools": "HTML, CSS, Javascript, PHP, Laravel, Angular, Vue, React, Wordpress, Nodejs"
+      },
+      {
+        "CompanyName":"MEF University",
+        "specialization":"Freelance Fullstack Developer",
+        "MonthOfLeaving":"November",
+        "YearOfLeaving":"2021",
+        "Tools": "JIRA, Figma, React, Nodejs, MongoDB, Javascript",
+        "Achievements":"Implementing an educational app for annotation and social reading"
+      },
+      {
+        "CompanyName":"Teleperformance Global",
+        "specialization":"Global Auditor",
+        "MonthOfLeaving":"September",
+        "YearOfLeaving":"2019",
+        "Achievements":"Conducting information security and operational audits, health&safety inspections over 35 countries with more than 70 visits"
+      },
+      {
+        "CompanyName":"Ege University SERLAB",
+        "specialization":"Researcher & Software Developer",
+        "MonthOfLeaving":"November",
+        "YearOfLeaving":"2013",
+        "Tools": "C/C++, Linux, ns-3, Java",
+        "Achievements":"Designed a Multi-Agent-System architecture for P2P video streaming. Contributed to several IEEE proceedings & SCI journal"
+      },
+      {
+        "CompanyName":"TT Atelier",
+        "specialization":"Web Design&Development",
+        "MonthOfLeaving":"November",
+        "YearOfLeaving":"2011",
+        "Tools": "HTML, CSS, Javascript, JQuery, .NET, SQL, Photoshop",
+      },
+    ],
+    "skillsDescription":"Angular, Vue, PHP, Laravel, .NET, SQLite, MSSQL, Photoshop, InDesign, Jira, Figma, Redux, Prisma, Ant Design, Material UI",
+    "skills":[
+      {
+        "skillname":"React"
+      },
+      {
+        "skillname":"Nodejs"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Typescript"
+      },
+      {
+        "skillname":"Postgres"
+      },
+      {
+        "skillname":"MongoDB"
+      },
+      {
+        "skillname":"HTML-CSS"
+      },
+      {
+        "skillname":"C"
+      },
+      {
+        "skillname":"Java"
+      },
+      {
+        "skillname":"Git"
+      },
+      {
+        "skillname":"Docker"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
